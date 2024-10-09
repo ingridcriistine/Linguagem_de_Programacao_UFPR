c32622a0c8425084536d16abfa945ef6f1b1a8fe
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2024-10-09 20:56:51 -0300

    Create soma2.c

---
+++ Av1/soma2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int A;
+    int B;
+    
+    scanf("%i", &A);
+    scanf("%i", &B);
+    
+    int X = A+B;
+    
+    printf("X = %i\n", X);
+    
+    return 0;
+}
